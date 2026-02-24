dedbe6ef5dfab57a8fc938113b39a90b9e8bff45
Andreea <144158945+SanduAndreea22@users.noreply.github.com>
2026-02-24 23:00:12 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', include('website.urls')),  # leagă site-ul tău
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
